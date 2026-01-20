744de96765d1f1b3d0a05e575d1842c08752d6e3
krendelёk <139132017+nastuh@users.noreply.github.com>
2026-01-20 13:51:46 +0300

    Delete Calculator directory

--- Calculator/Calculator/Calculator.cpp
+++
@@ -1,9 +0,0 @@
-#include "Calculator.h"
-#include <Windows.h>
-using namespace DefCalculator; 
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew Calculator);
-	return 0;
-}
